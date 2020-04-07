d0db9b981b8c0af22fd747d441b04b0a0f9e67e4
lyx <416698547@qq.com>
2020-04-07 10:22:23 +0800

    添加template

--- consumer-order/src/test/java/com/lyx/AppTest.java
+++ consumer-order/src/test/java/com/lyx/AppTest.java
@@ -1,5 +1,27 @@
 package com.lyx;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.client.RestTemplate;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class AppTest
 {
+	@Autowired
+	@Qualifier("restTemplate")
+	private RestTemplate template;
+
+	@Test
+	public void test1()
+	{
+		ResponseEntity<String> responseEntity = template.getForEntity("https://www.baidu.com", String.class);
+
+		System.out.println(responseEntity.getBody());
+	}
 }
