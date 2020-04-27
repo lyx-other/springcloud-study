f2f5cb3dbe987d41ae93b2f68d694b8ff18484ea
lyx <416698547@qq.com>
2020-04-27 18:40:43 +0800

    添加服务发现

--- m1/src/main/java/com/payment/M1App.java
+++ m1/src/main/java/com/payment/M1App.java
@@ -2,10 +2,12 @@ package com.payment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableEurekaClient
 @SpringBootApplication
+@EnableDiscoveryClient
 public class M1App
 {
 	public static void main(String[] args)
--- m1/src/main/java/com/payment/controller/TestController.java
+++ m1/src/main/java/com/payment/controller/TestController.java
@@ -1,12 +1,26 @@
 package com.payment.controller;
 
 import com.lyx.ResponseBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class TestController
 {
+	@Autowired
+	private DiscoveryClient discoveryClient;
+
+	@GetMapping("/discover")
+	public ResponseBean discover()
+	{
+		List<String> services = discoveryClient.getServices();
+		return ResponseBean.successData(discoveryClient);
+	}
+
 	@GetMapping("/foo")
 	public ResponseBean foo()
 	{
