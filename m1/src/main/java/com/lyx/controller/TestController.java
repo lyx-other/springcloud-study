c6f58b7ba39875f4f51516406930404897a6f04f
lyx <416698547@qq.com>
2020-04-29 10:51:47 +0800

    a

--- m1/src/main/java/com/lyx/controller/TestController.java
+++ m1/src/main/java/com/lyx/controller/TestController.java
@@ -1,21 +1,17 @@
 package com.lyx.controller;
 
-import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.serializer.SerializerFeature;
 import com.lyx.ResponseBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.cloud.client.ServiceInstance;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.List;
-
 @RestController
 public class TestController
 {
+	private static final String PAYMENT_URL = "http://M2";
+
 	@Autowired
 	@Qualifier("restTemplate")
 	private RestTemplate restTemplate;
@@ -25,6 +21,6 @@ public class TestController
 	{
 		System.out.printf("执行了m1模块的foo方法");
 
-		return restTemplate.getForObject("http://127.0.0.1/foo", ResponseBean.class);
+		return restTemplate.getForObject(PAYMENT_URL + "/foo", ResponseBean.class);
 	}
 }
--- m2/src/main/java/com/lyx/controller/TestController.java
+++ m2/src/main/java/com/lyx/controller/TestController.java
@@ -1,15 +1,19 @@
 package com.lyx.controller;
 
 import com.lyx.ResponseBean;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController
 {
+	@Value("${server.port}")
+	private String port;
+
 	@GetMapping("/foo")
 	public ResponseBean foo()
 	{
-		return ResponseBean.successMsg("是这m2模块的 foo 接口");
+		return ResponseBean.successMsg("是这m2模块的 foo 接口，使用的接口是 " + port);
 	}
 }
