07b243572b4fc128e5a8a44f240e40837b784ee8
lyx <416698547@qq.com>
2020-04-30 14:55:59 +0800

    负载均稀

--- m1/src/main/java/com/ribbon/RibbonRule.java
+++ m1/src/main/java/com/ribbon/RibbonRule.java
@@ -8,7 +8,9 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class RibbonRule
 {
-	@Bean
+	// 一个类只配置这么一个bean
+	// 因为在后边添加的时候用到的时 类的名称 而不是bean的名称
+	@Bean("iRule")
 	public IRule getRule()
 	{
 		return new RandomRule();
